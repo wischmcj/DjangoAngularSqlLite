17c16895b7ec8612ee59c88502c6d9356a43dd42
wischmcj <wischmcj@miamioh.edu>
2021-07-13 19:02:34 -0400

    integrating producer with app
    starting to try to log to kafka from the app directly.  Currently unsucessful.

--- DjangoAPI/ConsumerRunner/producer.py
+++ DjangoAPI/ConsumerRunner/producer.py
@@ -4,16 +4,30 @@ from django.http import HttpResponse
 import pickle
 
 def kfk():  
-    print("produce") 
     producer = KafkaProducer(bootstrap_servers='127.0.0.1:9092')
     v = {
         'msg': {
-            'I am': 'Jeff',
+            'I am': 'JeffNow',
         },
     }
     serialized_data = pickle.dumps(v, pickle.HIGHEST_PROTOCOL)
     producer.send('Ptopic', serialized_data)
     return HttpResponse(200)
+
+
+# def EmployeeUpdate():  
+#     producer = KafkaProducer(bootstrap_servers='127.0.0.1:9092')
+#     v = {
+#         'msg': {
+#             'I am': 'Jeff',
+#         'msg': {
+#             'I am': 'Jeff',
+        
+#         },
+#     }
+#     serialized_data = pickle.dumps(v, pickle.HIGHEST_PROTOCOL)
+#     producer.send('Ptopic', serialized_data)
+#     return HttpResponse(200)
 
 def main():
     kfk()
--- README.txt
+++ README.txt
@@ -3,3 +3,21 @@ Env (the python environment) first
 Got to ../scripts and run 'activate'
 
 You can then navigate to ../DjangoAPI and run the typical 'python manage.py runserver' command
+
+
+Commands to run Kafka
+
+C:\Users\wisch\kafka\kafka_2.13-2.6.0
+
+.\bin\windows\zookeeper-server-start.bat .\config\zookeeper.properties
+
+.\bin\windows\kafka-server-start.bat .\config\server.properties
+
+To activate env and nav to app
+
+activate 
+cd ..
+cd ..
+cd DjangoApi
+
+python manage.py runserver --noreload
